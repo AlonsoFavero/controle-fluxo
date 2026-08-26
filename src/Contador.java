d84de3f2c7f7f3d077a1a7235a2bd94c6c8feb24
AlonsoFavero <alonso.favero@gmail.com>
2026-08-26 12:57:36 -0300

    Cria estrutura inicial do contador e leitura dos parâmetros

---
+++ src/Contador.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numeros1 = scanner.nextInt();
+        int numeros2 = scanner.nextInt();
+
+    }
+}
